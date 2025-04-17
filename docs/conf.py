c2d5ca73583a1ee38f194cd02310dae59679d228
caz1que <okayokayokayigotcha@gmail.com>
2025-04-17 16:21:16 +0300

    feat: add markdown support for rtd

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,14 @@ extensions = []
 templates_path = ['_templates']
 exclude_patterns = []
 
+extensions = [
+    'myst_parser',
+]
 
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+}
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
