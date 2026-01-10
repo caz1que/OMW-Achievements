6e9481f3679348655f7d4fdf7fbb1652c2f98629
Skooma Lover <104842811+caz1que@users.noreply.github.com>
2026-01-10 13:42:32 +0300

    feat: add openmw logo to docs

--- docs/conf.py
+++ docs/conf.py
@@ -40,3 +40,12 @@ html_static_path = ['_static']
 html_theme_options = asdict(ThemeOptions(
    awesome_headerlinks=False
 ))
+
+# The name of an image file (relative to this directory) to place at the top
+# of the sidebar.
+html_logo = 'https://gitlab.com/OpenMW/openmw-docs/raw/master/docs/source/_static/images/openmw.png'
+
+# The name of an image file (within the static path) to use as favicon of the
+# docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
+# pixels large.
+html_favicon = 'https://gitlab.com/OpenMW/openmw-docs/raw/master/docs/source/_static/images/favicon.png'
